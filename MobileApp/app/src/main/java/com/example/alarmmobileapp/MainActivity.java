abe65085431de9d0eae0d3f7a82cb14a615e639e
PostnikovEvgeny <postnikoff.evgeny2015@yandex.ru>
2024-10-22 20:22:00 +0500

    Реализован интерфейс для выбора времени сигнализации

--- MobileApp/app/src/main/java/com/example/alarmmobileapp/MainActivity.java
+++ MobileApp/app/src/main/java/com/example/alarmmobileapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.alarmmobileapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    Button setAlarm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        setAlarm= findViewById(R.id.alarmButton);
+
+        setAlarm.setOnClickListener(v->{
+            MaterialTimePicker materialTimePicker = new MaterialTimePicker.Builder()
+                    .setTimeFormat(TimeFormat.CLOCK_24H)
+                    .setHour(12)
+                    .setMinute(0)
+                    .setTitleText("Выберите время для сигнализации")
+                    .build();
+
+            materialTimePicker.addOnPositiveButtonClickListener(view->{
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.SECOND, 0);
+                calendar.set(Calendar.MILLISECOND, 0);
+                calendar.set(Calendar.MINUTE, materialTimePicker.getMinute());
+                calendar.set(Calendar.HOUR_OF_DAY, materialTimePicker.getHour());
+            });
+
+            materialTimePicker.show(getSupportFragmentManager(), "myTag");
         });
     }
 }
